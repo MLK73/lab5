aa0545aa2efff772d757fe8d1bf98895796cde6f
sneuser <sneuser@snemaster>
2015-04-17 12:05:13 -0500

    5a en 5b

---
+++ lab5/lab5b.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+def function_show_my_name():
+    print("Micha :-)")
+    
+function_show_my_name()
+        
+def product():
+    a = int(input("geef een integer"))
+    b = int(input("geef nog een integer"))
+    return a * b
